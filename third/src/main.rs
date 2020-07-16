64635f3dbeee10d28bf5d92e99a1be6ddd5082cf
Saad Khan <Saadk2747@gmail.com>
2020-07-16 20:04:00 +0500

    cargo.toml and main.rs file are commited

---
+++ third/src/main.rs
@@ -0,0 +1,9 @@
+use std::io::{stdin,stdout,write};
+fn ready(input:& String){
+    stdout().flush().expect("sorry");
+    stdin().read_line(input).expect("your error occured");
+    
+}
+fn main (){
+
+}
