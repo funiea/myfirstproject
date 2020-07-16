0ccf7032f85dae39720c57d638cbc753159b0281
funiea <63172001+funiea@users.noreply.github.com>
2020-07-16 20:28:13 +0500

    Updated third.txt
    changes from github master main  repository

--- third/src/main.rs
+++ third/src/main.rs
@@ -5,5 +5,17 @@ fn ready(input:& String){
     
 }
 fn main (){
-
+    println!("welcome to khan calculator");
+    
+    let num1 = String::new();
+    let num2 = String::new();
+    let operator = String::new();
+    
+    println!("type first number ?");
+    ready(&mut num1);
+    println!("type your operator");
+    ready(&mut operator);
+    println!("type your second number ?");
+    ready(&mut num2)
+    
 }
